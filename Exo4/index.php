e510e1d617184bdfb39c9e9eec982a6f6671a5a2
EmyDvger <emilieduverger_80@yahoo.com>
2019-12-02 16:44:40 +0100

    Exo PHP fait

---
+++ Exo4/index.php
@@ -0,0 +1,15 @@
+<?php
+//déclaration de ma variable name
+$name = 'Emilie'
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8"/>
+  <title>PHP partie 1 Exercice 4</title>
+</head>
+<body>
+  <h1>Exo 4 PHP</h1>
+  <p>Bonjour <?php echo $name ?> , comment vas tu ?</p>
+</body>
+</html>
