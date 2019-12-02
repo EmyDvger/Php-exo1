71aec467b7247ed967fa49f5bf23d95f267a0854
EmyDvger <emilieduverger_80@yahoo.com>
2019-12-02 16:09:53 +0100

    Exo1

---
+++ Exo3/index.php
@@ -0,0 +1,16 @@
+<?php
+$km = 1;
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>PHP partie 1 Exercice 3</title>
+</head>
+<body>
+  <h1>Exo 3 PHP</h1>
+  <p><?php echo $km;?></P>
+  <p><?php echo $km=3;?></P>
+  <p><?php echo $km=125;?></P>
+  </body>
+</html>
