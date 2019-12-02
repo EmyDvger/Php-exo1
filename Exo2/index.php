59746d3c3a14962072397d1525a12c894e43f5d4
EmyDvger <emilieduverger_80@yahoo.com>
2019-12-02 15:08:23 +0100

    PHP Partie 1 Exo 2

---
+++ Exo2/index.php
@@ -0,0 +1,18 @@
+<?php
+$lastname = 'Duverger';
+$firstname = 'Emilie';
+$old = 35;
+ ?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP partie 1 Exercice 2</title>
+  </head>
+  <body>
+    <h1>Exo 2 PHP</h1>
+    <p><?php echo $lastname;?></p>
+    <p><?php echo $firstname;?></p>
+    <p><?php echo $old;?> ans</p>
+  </body>
+</html>
