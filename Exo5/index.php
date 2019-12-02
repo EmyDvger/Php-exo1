3186a733221f71842ba31ee67d241104e439bb90
EmyDvger <emilieduverger_80@yahoo.com>
2019-12-02 17:01:32 +0100

    exo5

---
+++ Exo5/index.php
@@ -0,0 +1,21 @@
+<?php
+//déclaration de ma variable
+//créer une condition
+$answer = 'yes'
+if ($answer = 'yes')
+  {
+    echo "Vous avez repondu oui"
+  }
+ ?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8"/>
+    <title>PHP Partie 1 Exercice 5</title>
+  </head>
+  <body>
+  <p>
+    <?php echo $answer= ?>
+  </p>
+  </body>
+</html>
