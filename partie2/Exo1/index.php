d30e8a5c97a63902c9bcc8e878cdfde606544d97
EmyDvger <emilieduverger_80@yahoo.com>
2019-12-03 11:08:51 +0100

    creation dossier partie 2

---
+++ partie2/Exo1/index.php
@@ -0,0 +1,14 @@
+<?php  ?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>PHP partie 2 Exo 1</title>
+  </head>
+  <body>
+  <p>
+<?php
+ ?>
+  </p>
+  </body>
+</html>
