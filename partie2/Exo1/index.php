f2b59b07b9a298f0978a85885920e812f41b7caf
EmyDvger <emilieduverger_80@yahoo.com>
2019-12-03 14:07:28 +0100

    Exercice 1 partie 2 fait

--- partie2/Exo1/index.php
+++ partie2/Exo1/index.php
@@ -1,13 +1,23 @@
-<?php  ?>
+<?php
+$old = 19 ;
+ ?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
   <head>
-    <meta charset="utf-8">
+    <meta charset="utf-8"/>
     <title>PHP partie 2 Exo 1</title>
   </head>
   <body>
   <p>
 <?php
+if ($old >= 18)//  SI l'âge est supérieur ou égal à 18
+{
+  echo 'Vous êtes majeur !';
+}
+else
+{
+  echo 'Vous êtes mineur !';
+}
  ?>
   </p>
   </body>
