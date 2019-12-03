400b7ec1a8d5c50e91aea877fe787bf0e194fff8
EmyDvger <emilieduverger_80@yahoo.com>
2019-12-03 11:02:04 +0100

    exo 5 et exo 6 fait

--- Exo5/index.php
+++ Exo5/index.php
@@ -1,21 +1,26 @@
 <?php
 //déclaration de ma variable
-//créer une condition
-$answer = 'yes'
-if ($answer = 'yes')
-  {
-    echo "Vous avez repondu oui"
-  }
- ?>
+$answer = 'yes';
+?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
-  <head>
-    <meta charset="utf-8"/>
-    <title>PHP Partie 1 Exercice 5</title>
-  </head>
-  <body>
+<head>
+  <meta charset="utf-8"/>
+  <title>PHP Partie 1 Exercice 5</title>
+</head>
+<body>
+  <!-- html body qui permet afficher , mettre toujours des simples côte en PHP, le == signifie comparaison-->
   <p>
-    <?php echo $answer= ?>
+    <?php
+    if ($answer == 'yes')
+    {
+      echo 'Vous avez répondu Oui !';
+    }
+    else
+    {
+      echo 'Vous avez répondu Non !';
+    }
+    ?>
   </p>
-  </body>
+</body>
 </html>
---
+++ Exo6/index.php
@@ -0,0 +1,17 @@
+<?php
+//déclaration de ma variable
+//créer une condition
+$number = 140;
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8"/>
+    <title>PHP Partie 1 Exercice 6</title>
+  </head>
+  <body>
+    <p>
+      <?php echo (($number + 30) / 2) ?>
+    </p>
+  </body>
+</html>
