6c32a58b6e28a676d786e762be3a11d8cdf02ad6
EmyDvger <emilieduverger_80@yahoo.com>
2019-12-03 15:06:32 +0100

    exo fait

---
+++ partie3/Exo2/index.php
@@ -0,0 +1,17 @@
+<?php
+$months = array ('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre',
+ 'Octobre', 'Novembre', 'Décembre');
+ ?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8"/>
+    <title>PHP Partie 3 Exo 2</title>
+  </head>
+  <body>
+  <h1>Exo 2 partie 3</h1>
+  <p>
+    <?php echo $months[5]; ?>
+  </p>
+  </body>
+</html>
