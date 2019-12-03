9338133e95f35c96cf6ca01ef0a7a64bce9520e6
EmyDvger <emilieduverger_80@yahoo.com>
2019-12-03 16:40:24 +0100

    correction exo

--- Exo6/index.php
+++ Exo6/index.php
@@ -10,8 +10,22 @@ $number = 140;
     <title>PHP Partie 1 Exercice 6</title>
   </head>
   <body>
+    <!-- Affichage du résulat souhaité grâce à echo -->
+    <!-- présentation des différentes méthodes -->
     <p>
       <?php echo (($number + 30) / 2) ?>
     </p>
+    <p>
+      <?php $number = $number + 30;
+    $number = $number / 2;
+    echo $number; ?>
+  </p>
+  <p>
+    <?php
+     $number += 30;
+     $number /= 2;
+     echo $number;
+     ?>
+   </p>
   </body>
 </html>
